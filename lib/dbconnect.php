e39b5976ca50c5619e5d4a2e68c09b0439ec403f
Vasilis Katseisvili <vasilis1908shako@gmail.com>
2024-01-10 14:44:05 +0200

    conn 4

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -11,6 +11,7 @@ $user_iee=$DB_USER_IEE;
 
 
 if(gethostname()=='users.iee.ihu.gr') {
+    $pass=null;
 	$mysqli = new mysqli($host, $user_iee, $pass, $db,null,'/home/student/iee/2019/iee2019066/mysql/run/mysql.sock');
 } else {
 		$pass=null;
